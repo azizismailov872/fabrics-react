f130f140ccc04ffcdf0436dcf9d757e9011d32a0
Aziz Ismailov <azizismailov872872@gmail.com>
2023-05-11 22:08:15 +0600

    Создал компоненты для функционала по редактированию тканей

--- src/pages/Fabrics/Update/FabricsUpdate.jsx
+++ src/pages/Fabrics/Update/FabricsUpdate.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import { Modal } from '@mui/material';
+import { Controller } from 'react-hook-form';
+import Button from '../../../components/Button/Button';
+import Input from '../../../components/Form/Input/Input';
+import ColorForm from '../../../components/Forms/Color/ColorForm';
+import { Calculator, Dna, Hash, Palette } from "@phosphor-icons/react"
+import SelectChip from '../../../components/Form/SelectChip/SelectChip';
 
-const FabricsUpdate = () => {
-  return (
-    <div>FabricsUpdate</div>
-  )
+const FabricsUpdate = ({ register, control, errors, colors, onSubmit, onReset, title, modal, openModal, closeModal }) => {
+
+    return (
+        <div className='pb-8 pt-2'>
+            <div className='flex justify-between mb-5 items-center'>
+                <h2 className='text-xl font-medium md:text-2xl  dark:text-primary-light text-dark-100'>{title}</h2>
+                <button onClick={() => openModal('createColor')} className='text-stone-400'>Добавить цвет +</button>
+            </div>
+            <form onSubmit={onSubmit}>
+                <div className='flex flex-col md:flex-row md:flex-wrap gap-3'>
+                    <div className='grow md:grow-0 shrink-0 md:basis-[17rem]'>
+                        <Input
+                            name="model"
+                            label="Модель"
+                            icon={Hash}
+                            inputProps={{ ...register('model') }}
+                            size="small"
+                            className="w-full"
+                            error={!!errors.model}
+                            errorMessage={errors?.model?.message && errors.model.message}
+                        />
+                    </div>
+                    <div className='grow md:grow-0 shrink-0 md:basis-[17rem]'>
+                        <Input
+                            name="quantity"
+                            label="Колличество"
+                            icon={Calculator}
+                            inputProps={{ ...register('quantity') }}
+                            size="small"
+                            type="number"
+                            className="w-full"
+                            error={!!errors.quantity}
+                            errorMessage={errors?.quantity?.message && errors.quantity.message}
+                        />
+                    </div>
+                    <div className='grow md:grow-0 shrink-0 md:basis-[17rem]'>
+                        <Input
+                            name="materials"
+                            label="Материалы"
+                            icon={Dna}
+                            inputProps={{ ...register('materials') }}
+                            size="small"
+                            className="w-full"
+                            error={!!errors.materials}
+                            errorMessage={errors?.materials?.message && errors?.materials?.message}
+                        />
+                    </div>
+                    <div className='grow md:grow-0 shrink-0 md:basis-[17rem]'>
+                        <Controller
+                            render={({ field }) => (
+                                <SelectChip
+                                    {...field}
+                                    size="small"
+                                    label="Цвета"
+                                    className="w-full"
+                                    icon={Palette}
+                                    minWidth="180px"
+                                    optionLabel="name"
+                                    options={
+                                        colors?.data?.data ? colors.data.data : []
+                                    }
+                                    error={!!errors.colors}
+                                    errorMessage={errors?.colors?.message && errors?.colors?.message}
+                                />
+                            )
+                            }
+                            name="colors"
+                            control={control}
+                            defaultValue={[]}
+                        />
+                    </div>
+                </div>
+                <div className='mt-4 flex gap-3'>
+                    <Button type="submit">Отправить</Button>
+                    <Button onClick={onReset} type="button" secondary>Очистить</Button>
+                </div>
+            </form>
+            <Modal
+                open={modal.isOpen}
+                onClose={closeModal}
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <ColorForm onSuccessFn={closeModal} />
+            </Modal>
+        </div>
+    )
 }
-
 export default FabricsUpdate
---
+++ src/pages/Fabrics/Update/FabricsUpdateContainer.jsx
@@ -0,0 +1,105 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import { useNavigate, useParams } from 'react-router-dom';
+import { FabricsService } from '../../../services/FabricsService';
+import useModal from '../../../hooks/useModal';
+import { ColorsService } from '../../../services/ColorsService';
+import { toast } from 'react-hot-toast';
+import { setErrors } from '../../../helper';
+import { isArray, map } from 'lodash';
+import FabricsUpdate from './FabricsUpdate';
+import { updateSchema } from '../../../validation/fabrics';
+import FormLoader from '../../../components/Loaders/Form/FormLoader';
+
+const getFabricsColors = (fabricsColors) => {
+    return (isArray(fabricsColors) && fabricsColors.length > 0) ? map(fabricsColors, 'id') : []
+}
+
+const FabricsUpdateContainer = () => {
+
+    const {id} = useParams()
+
+    const navigate = useNavigate()
+
+    const queryClient = useQueryClient()
+
+    const [modal, openModal, closeModal] = useModal()
+
+    const mutation = useMutation({
+        mutationFn: FabricsService.update
+    })
+
+    const {register,control,handleSubmit,formState: {errors},reset,setError} = useForm({
+        resolver: yupResolver(updateSchema)
+    })
+
+    const {data: fabric,isLoading: isFabricLoading} = useQuery({
+        queryKey: ['getFabric',id],
+        queryFn: () =>  FabricsService.getFabric(id),
+        refetchOnWindowFocus: false,
+        onSuccess: fabric => {
+            if(fabric && fabric?.data?.data) {
+                reset({
+                    ...fabric.data.data,
+                    colors: getFabricsColors(fabric.data.data.colors)
+                })
+            }
+        }
+    })
+
+    const {data: colors, isLoading: isColorsLoading} = useQuery({
+        queryKey: ['getColorsList'],
+        queryFn: ColorsService.getColorsList,
+        refetchOnWindowFocus: false,
+    })
+
+    const onSubmit = async(formData) => {
+        const loadingTost = toast.loading('Сохранение...')
+        mutation.mutate({id,data: formData},{
+            onError: (error, variables, context) => {
+                console.log('error: ',error)
+                toast.error('Ошибка при сохранении данных',{
+                    id: loadingTost
+                });
+                if (error.response.data?.errors) {
+                    setErrors(error.response.data.errors, setError)
+                }
+            },
+            onSuccess: (data, variables, context) => {
+                const message = data.data.message ? data.data.message : 'Данные успешно сохранены2'
+                toast.success(message, {
+                    id: loadingTost,
+                });
+                queryClient.invalidateQueries('getFabric')
+                navigate('/fabrics')
+            },
+        })
+    }
+
+    const onReset = () => {
+        reset({
+            model: null,
+            quantity: null,
+            materials: null,
+            colors: []
+        })
+    }
+
+    return (isFabricLoading || isColorsLoading) ? <FormLoader /> : (
+        <FabricsUpdate 
+            register={register}
+            control={control}
+            errors={errors}
+            onSubmit={handleSubmit(onSubmit)}
+            onReset={onReset}
+            colors={colors}
+            title={`Редактировать ${fabric.data.data.model}`}
+            modal={modal}
+            openModal={openModal}
+            closeModal={closeModal}
+        />
+    )
+}
+
+export default FabricsUpdateContainer
